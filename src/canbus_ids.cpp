b9529c71d1a1cd5a3bb29c3f093e0cb2751e23b7
Gavin Hurlbut <gjhurlbu@gmail.com>
2023-03-03 10:24:49 -0500

    Make the canbus ID name thing only active if logging is on

--- src/canbus_ids.cpp
+++ src/canbus_ids.cpp
@@ -4,12 +4,15 @@
 
 const char *get_canbus_id_name(int id)
 {
+#ifdef DISABLE_LOGGING
+  (void)id;
+#else
   canbus_id_t canbus_id = static_cast<canbus_id_t>(id);
   const char *name = CANBUS_ID_NAME(canbus_id);
   if (name) {
     return name + 10;
   }
+#endif
   return 0;
 }
-
 
