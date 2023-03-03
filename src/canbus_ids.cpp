c7442ac2155f7c32cada4091e135f838efd7b9ae
Gavin Hurlbut <gjhurlbu@gmail.com>
2023-03-02 21:38:44 -0500

    Maybe fix the sensor name.

--- src/canbus_ids.cpp
+++ src/canbus_ids.cpp
@@ -5,7 +5,7 @@
 const char *get_canbus_id_name(int id)
 {
   canbus_id_t canbus_id = static_cast<canbus_id_t>(id);
-  const char *name = CANBUS_ID_NAME(x);
+  const char *name = CANBUS_ID_NAME(canbus_id);
   if (name) {
     return name + 10;
   }
